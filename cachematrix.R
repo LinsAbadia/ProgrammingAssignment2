b6df57226ad2ed24feefcb0ce6963df379b407c0
Lindsley Abadia <LinsAbadia@users.noreply.github.com>
2021-12-20 14:08:22 +1030

    cachematrrix.R with comments 2021 Dec. 20

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,18 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+##   Per the assignment requirements, the cachematrix.R script implement the two (2) functions
+##    requested in this branch: makeCacheMarix and ccheSolve.  These essentially, caches
+##    the inverse of a supplied matrix.  To test these, user-defined, 'singular' matrices 
+##    (and their inverses) and compared against each functions internalvariables and
+##    functions (basically, a list composed of four (4) elements).  Descriptions are given 
+##    before each function definition.
 
 ## Write a short comment describing this function
+##  This is adapted from the supplied MakeVector.R and insteads 'caches' the matrix provided.
+##  It changes the following:  m (mean) to s (solve),  setmean to setMInv,
+##   getmean to getMInv, and "function(mean) m <<- mean" to "function(solve) s <<- solve".
+
 makeCacheMatrix <- function(x = matrix()) {
-  
   s <- NULL
   set <- function(y) {
     x <<- y
@@ -12,20 +21,21 @@ makeCacheMatrix <- function(x = matrix()) {
   get <- function() x
   setMInv <- function(solve) s <<- solve
   getMInv <- function() s
+  
   list(set = set, get = get,
        setMInv = setMInv,
-       getMInv = getMInv()
+       getMInv = getMInv)
 }
 
-
-}
-
-
-
+#  This is adapted from the supplied CacheMean.R and insteads 'caches' the matrix provided.
+#  It changes the following:  m (mean) to s (solve),  setmean to setMInv,
+#   getmean to getMInv, data to mData, " message("getting cached data")" to 
+#   "message("getting cached matrix data")". and "m <- mean(data, ...)" to
+#    "s <- solve(mData, ...)".
 cacheSolve <- function(x, ...) {
   s <- x$getMInv()
   if(!is.null(s)) {
-    message("getting cached matrixdata")
+    message("getting cached matrix data")
     return(s)
   }
   mData <- x$get()
